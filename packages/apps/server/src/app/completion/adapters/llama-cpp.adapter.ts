eb8de39fd746a2f342dd5924c22b140fea50976f
Louis Brunet <louis.jc.brunet@gmail.com>
2025-03-31 21:20:12 +0200

    test(apps/server): fix broken tests

--- packages/apps/server/src/app/completion/adapters/llama-cpp.adapter.ts
+++ packages/apps/server/src/app/completion/adapters/llama-cpp.adapter.ts
@@ -2,15 +2,13 @@ import {
   LlamaCppConfig,
   LlamaCppConfigService,
 } from '#config/configurations/llama-cpp-config.service';
-import { LlamaCppClient, LlamaCppInfillResponse } from '@llm-tools/clients';
+import {
+  LlamaCppClient,
+  LlamaCppInfillResponse,
+  LlamaCppModelEnum,
+} from '@llm-tools/clients';
 import { Injectable, Logger } from '@nestjs/common';
 import { InfillRequest } from '../interfaces';
-
-// const infillResponseSchema = object({
-//   content: string().required(),
-//   tokens_predicted: number().integer().required(),
-// });
-// export type LlamaCppInfillResponse = InferType<typeof infillResponseSchema>;
 
 @Injectable()
 export class LlamaCppAdapter {
@@ -22,6 +20,7 @@ export class LlamaCppAdapter {
     this.config = configService.getConfig();
     this.llamaCppClient = new LlamaCppClient({
       serverOrigin: this.config.serverOrigin,
+      model: LlamaCppModelEnum.QWEN_2_5_CODER,
     });
   }
 
@@ -32,43 +31,11 @@ export class LlamaCppAdapter {
         filename: item.fileName,
         text: item.content,
       })) ?? [];
-    // this.logger.debug(`[infill] ${JSON.stringify({ inputExtra })}`);
     return await this.llamaCppClient.infill({
       input_prefix: request.before,
       input_suffix: request.after,
       input_extra: inputExtra,
       prompt: request.prompt,
     });
-
-    // // https://github.com/ggml-org/llama.cpp/blob/master/examples/server/README.md#post-infill-for-code-infilling
-    // const response = await firstValueFrom(
-    //   this.httpService
-    //     .post(
-    //       `${this.config.serverOrigin}/infill`,
-    //       llamaRequest,
-    //       // { headers: { 'Content-Type': 'application/json' } },
-    //     )
-    //     .pipe(
-    //       tap((response) => {
-    //         this.logger.debug(`[infill] response status ${response.status}`);
-    //         // this.logger.debug(
-    //         //   `[infill] response ${JSON.stringify(response.data)}`,
-    //         // );
-    //       }),
-    //       // tap((response) => response.data),
-    //       catchError((error) => {
-    //         this.logger.error(`[infill] error ${JSON.stringify(error)}`);
-    //         this.logger.error(
-    //           `[infill] error response ${JSON.stringify((error as AxiosError)?.response?.data)}`,
-    //         );
-    //         throw error;
-    //         // return of({
-    //         //   content: '',
-    //         //   tokens_predicted: 0,
-    //         // });
-    //       }),
-    //     ),
-    // );
-    // return this.validationService.validate(infillResponseSchema, response.data);
   }
 }
